563439716ce6a34f3e20cea2d8d19267684277e4
JCM <jcl.maquinay@gmail.com>
2024-01-02 08:50:53 +0100

    Start todo be repository

--- libs/data/todos/src/lib/todos.module.ts
+++ libs/data/todos/src/lib/todos.module.ts
@@ -1,10 +1,13 @@
+import { PrismaModule } from '@my-app/prisma';
 import { Module } from '@nestjs/common';
 import { TodosController } from './todos.controller';
+import { TodosRepository } from './todos.repository';
 import { TodosService } from './todos.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [TodosController],
-  providers: [TodosService],
+  providers: [TodosRepository, TodosService],
   exports: [TodosService],
 })
 export class TodosModule {}
