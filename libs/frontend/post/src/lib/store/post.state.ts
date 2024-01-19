fe8c14334d29cd2a4e7306133f2ed226692a2ce2
JCM <jcl.maquinay@gmail.com>
2024-01-19 09:49:21 +0100

    Package upate

--- libs/frontend/post/src/lib/store/post.state.ts
+++ libs/frontend/post/src/lib/store/post.state.ts
@@ -1,4 +1,4 @@
-import { withDevtools } from "@angular-architects/ngrx-toolkit";
+
 import { effect } from "@angular/core";
 import { withCallState, withDataService, withLogger, withUndoRedo } from "@fe/shared/util-signal-store";
 import { signalStore, type, withHooks } from "@ngrx/signals";
@@ -18,7 +18,6 @@ export const initialPostState: PostStateInterface = {
 export const PostStore = signalStore(
   { providedIn: 'root' },
   // withState(initialPostState),
-  withDevtools('post'),
   withCallState({collection: 'post'}),
   withEntities( {entity: type<PostInterface>(), collection: 'post'}),
 
